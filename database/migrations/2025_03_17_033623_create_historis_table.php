c3c298a7376e6a9d72e121460a45108cbf190859
Wahyu Safrizal <wahyusafrizal174@gmail.com>
2025-06-29 16:25:35 +0700

    update

--- database/migrations/2025_03_17_033623_create_historis_table.php
+++ database/migrations/2025_03_17_033623_create_historis_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('histori', function (Blueprint $table) {
             $table->id();
-            $table->integer('document_id');
+            $table->string('document_id');
             $table->integer('user_id');
             $table->longText('sent');
             $table->longText('accepted');
